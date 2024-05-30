a56f23d0c3504bbd6d0fe4870806d276d5de546a
Cecilia Gio Alonso Goncalves <ceciggonca@gmail.com>
2024-05-29 21:13:54 -0300

    codigo final

--- backend/mover.py
+++ backend/mover.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
-
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 import sys, select, tty, termios
 import os
+from std_srvs.srv import Empty
 
 # Configurando velocidade
 MAX_LIN_VEL = 0.22
@@ -15,81 +14,26 @@ ANG_VEL_STEP_SIZE = 0.1
 class TeleopNode(Node):
     def __init__(self):
         super().__init__('teleop_node')
+        self.velocity = self.create_subscription(Twist,'/vel', self.callback_vel,10)
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.settings = termios.tcgetattr(sys.stdin)
+        self.service = self.create_service(Empty, '/kill_teleop_node', self.kill_node_callback)
         self.current_linear_vel = 0.0
         self.current_angular_vel = 0.0
-        self.get_logger().info( # Mensagem no terminal
-            """
-            Control Your Robot!
-            ---------------------------
-            Moving around:
-                ↑
-            ←       →
-                ↓
-            
-            ↑ : Mover para frente
-            ↓ : Mover para trás
-            ← : Mover para esquerda
-            → : Mover para direita
-            Tecla 'Espaço' : zera as velocidades
-            Tecla 'Enter' : encerra o programa
-            """
-        )
+        self.alive = True
+        
+    def callback_vel(self, msg):
+        self.publishTwist(msg)
 
-        self.timer = self.create_timer(0.1, self.update) # Atualização periódica
 
-    def update(self):
-        key = self.getKey() 
-        if key:
-            self.processKey(key)
+    def kill_node_callback(self, request, response):
+        self.get_logger().info('Kill node service called. Shutting down...')
+        self.alive = False
+        return response
 
-    def getKey(self): 
-        tty.setraw(sys.stdin.fileno()) # Leitura de tecla por tecla
-        rlist, _, _ = select.select([sys.stdin], [], [], 0.1) # Lê tecla pressionada
-        if rlist:
-            key = sys.stdin.read(1)
-            print(f"Key pressed: {key} ({ord(key)})")  
-            if key == '\x1b':  
-                additional_chars = sys.stdin.read(2)  
-                return key + additional_chars
-            return key
-        return ''
-
-    def processKey(self, key): # Processa as teclas
-        if key == '\x1b[A':  # Seta para cima
-            self.current_linear_vel = min(self.current_linear_vel + LIN_VEL_STEP_SIZE, MAX_LIN_VEL)
-            self.current_angular_vel = 0.0
-        elif key == '\x1b[B':  # Seta para baixo
-            self.current_linear_vel = max(self.current_linear_vel - LIN_VEL_STEP_SIZE, -MAX_LIN_VEL)
-            self.current_angular_vel = 0.0
-        elif key == '\x1b[D':  # Seta para esquerda
-            self.current_angular_vel = min(self.current_angular_vel + ANG_VEL_STEP_SIZE, MAX_ANG_VEL)
-            self.current_linear_vel = 0.0  # Zera a velocidade linear
-        elif key == '\x1b[C':  # Seta para direita
-            self.current_angular_vel = max(self.current_angular_vel - ANG_VEL_STEP_SIZE, -MAX_ANG_VEL)
-            self.current_linear_vel = 0.0 # Zera a velocidade linear
-        elif key == ' ':  # Tecla espaço
-            self.current_linear_vel = 0.0
-            self.current_angular_vel = 0.0
-            self.get_logger().info("Velocities zeroed.")
-        elif key == '\r' :  # Tecla Enter
-            print("Enter key detected, exiting...")
-            self.get_logger().info("Enter key pressed - shutting down.")
-            rclpy.shutdown()
-            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
-            os._exit(0)  # Encerra o programa 
-        else:
-            self.get_logger().info(f"Unrecognized key pressed: {key}")
-
-        self.publishTwist()
-
-    def publishTwist(self): # Mostra velocidade 
-        twist = Twist()
-        twist.linear.x = self.current_linear_vel
-        twist.angular.z = self.current_angular_vel
-        self.publisher.publish(twist)
-        self.get_logger().info(f"Linear Vel: {self.current_linear_vel}, Angular Vel: {self.current_angular_vel}")
+    def publishTwist(self,twist): # Mostra velocidade 
+        if self.alive: 
+            self.publisher.publish(twist)
+            self.get_logger().info(f"Linear Vel: {self.current_linear_vel}, Angular Vel: {self.current_angular_vel}")
 
 def main(args=None):
     rclpy.init(args=args)
--- backend/sender.py
+++ backend/sender.py
@@ -1,31 +1,39 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage
+from std_msgs.msg import Float32  # Adicionado para publicar a latência
 import cv2
+import time
 
 class Imagem(Node):
     def __init__(self):
         super().__init__('transmissao_imagem')
         self.publisher_ = self.create_publisher(CompressedImage, '/video_frames', 10)
+        self.latency_publisher_ = self.create_publisher(Float32, '/image_latency', 10)  # Publisher para latência
         self.timer = self.create_timer(0.03, self.timer_callback)
         self.cap = cv2.VideoCapture(0)
 
     def timer_callback(self):
+        start_time = time.time()  # Marcar o tempo de início
         ret, frame = self.cap.read()
         if ret:
-            print("Sending img")
             _, buffer = cv2.imencode('.jpg', frame)
             msg = CompressedImage()
             msg.format = "jpeg"
             msg.data = buffer.tobytes()
             self.publisher_.publish(msg)
+            
+            latency = (time.time() - start_time) * 1000  # Calcular a latência em milissegundos
+            latency_msg = Float32()
+            latency_msg.data = latency
+            self.latency_publisher_.publish(latency_msg)
+            self.get_logger().info(f'Sending image with latency: {latency:.2f} ms')
 
 def main(args=None):
     rclpy.init(args=args)
     transmissao_imagem = Imagem()
     try:
         rclpy.spin(transmissao_imagem)
-        print("spinning img")
     except KeyboardInterrupt:
         pass
     finally:
